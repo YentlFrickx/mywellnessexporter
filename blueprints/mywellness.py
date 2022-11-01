a5bbbc76bf01693adcd3adda90ca1142c556c84b
Yentl Frickx <frickx.yentl@gmail.com>
2022-11-01 14:25:24 +0100

    get_activity works

--- blueprints/mywellness.py
+++ blueprints/mywellness.py
@@ -1,10 +1,12 @@
+import re
+
 import requests
 from flask import Blueprint, render_template, redirect
 import pickle
 
 from flask_login import login_required, current_user
+from bs4 import BeautifulSoup
 
-from models import user
 from models.form import MyWellnessLoginForm
 from models.user import User
 
@@ -12,9 +14,11 @@ mywellness = Blueprint('mywellness', __name__)
 
 LOGIN_URL = 'https://www.mywellness.com/cloud/User/Login/'
 
+
 def save_cookies(requests_cookiejar, filename):
     with open(filename, 'wb') as f:
         pickle.dump(requests_cookiejar, f)
+
 
 def load_cookies(filename):
     with open(filename, 'rb') as f:
@@ -39,13 +43,38 @@ def login():
     return render_template('wellnesslogin.html', form=form)
 
 
-@mywellness.route('/connect/mywellness/activity')
+@mywellness.route('/connect/mywellness/sessions')
 @login_required
-def get_activity():
+def get_sessions():
     cookie_value = User.get_cookie(current_user.id)
     if not cookie_value:
         return redirect('/connect/mywellness')
-    cookie_dict={"_mwappseu": cookie_value}
-    url = 'https://www.mywellness.com/cloud/Training/PerformedExerciseDetail/?idCr=1003&position=2&dayOpenSession=20221029&singleView=False'
+    cookie_dict = {"_mwappseu": cookie_value}
+    url = 'https://www.mywellness.com/cloud/Training/LastPerformedWorkoutSession/?fromDate=27/10/2022&toDate=27/10/2022'
     response = requests.get(url=url, cookies=cookie_dict)
+    soup = BeautifulSoup(response.text, "html.parser")
+    sessions = soup.findAll('div', class_='single-item')
+    sessionIds = []
+    for session in sessions:
+        href = session.find_next('a')
+        return get_activity(href.get('href'), cookie_dict)
     return response.text
+
+
+def get_activity(url, cookie_dict):
+    response = requests.get(url=url, cookies=cookie_dict)
+    soup = BeautifulSoup(response.text, "html.parser")
+    js_scripts = soup.findAll('script', {"type": "text/javascript"})
+
+    activity_analytics_id = ''
+    token = ''
+
+    for script in js_scripts:
+        if script.text.find('EU.currentUser') > 0:
+            token = re.search("token..:.\"(.*?).\"", script.text).group(1)
+        elif script.text.find('window.physicalActivityAnalyticsId') > 0:
+            activity_analytics_id = re.search("window.physicalActivityAnalyticsId = '(.*?)'", script.text).group(1)
+
+    url = f"https://services.mywellness.com/Training/CardioLog/{activity_analytics_id}/Details?token={token}"
+    activity_response = requests.get(url=url, cookies=cookie_dict)
+    return activity_response.text
